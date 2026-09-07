fa65ea8284a5f7857c4afcf8b683cb95f39591cb
Emmanuel Fasina <emmanuelfasina@gmail.com>
2026-09-07 23:07:38 +0100

    Fix uploaded profile image serving

--- future_leaders_academy/urls.py
+++ future_leaders_academy/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('portal.urls')),
     path('admin/', admin.site.urls),
 ]
 
-# Serve media files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+]
