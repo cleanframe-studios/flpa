27024a214ea82074877db5409af74b327fadfa4e
Emmanuel Fasina <emmanuelfasina@gmail.com>
2026-09-08 11:28:47 +0100

    Use Render persistent disk for media

--- future_leaders_academy/settings.py
+++ future_leaders_academy/settings.py
@@ -136,7 +136,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Media files configuration for uploaded passports
 MEDIA_URL = os.environ.get('MEDIA_URL', '/media/')
-MEDIA_ROOT = Path(os.environ.get('MEDIA_ROOT', str(BASE_DIR / 'media')))
+default_media_root = '/var/data/media' if os.environ.get('RENDER') else str(BASE_DIR / 'media')
+MEDIA_ROOT = Path(os.environ.get('MEDIA_ROOT', default_media_root))
 
 # Email configuration
 # Defaults to printing emails to the console in development. Set real SMTP
